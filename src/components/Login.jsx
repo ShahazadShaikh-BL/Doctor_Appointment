401268c4bdc4cf251f81876f43367396cf930111
ShahazadShaikh-BL <shaikh.shahazad@bridgelabz.com>
2023-08-28 17:03:28 +0530

    Grid functionality Working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import "../styles/login.scss";
+import {Grid} from "@mui/material"
+import Login_Image from "../assests/Login_Image"
+const Login = () => {
 
-const Login=()=> {
-    
-    const handleSubmit=(e) => {
+    const handleSubmit = (e) => {
         console.log("Event Log...");
     }
-    return(
+    return (
         <div>
-           Login Page
+            <div class="container">
+                <div class="wrapper">
+                    <div class="box a">.a</div>
+                    <Grid container >
+                   
+                    <Grid item xs={8}>
+                    <Grid item container xs={4}>
+                    <img alt=" " src={Login_Image} />
+                    </Grid>
+  </Grid>
+                    </Grid>
+                </div>
+            </div>
         </div>
-   
-     );
-  };
-  
-  
+    );
+};
+
+
 export default Login;
--- src/components/Registeration.jsx
+++ src/components/Registeration.jsx
@@ -3,6 +3,7 @@ const Registeration=()=> {
     return(
         <div>
             <h1>Registeration Page</h1>
+            <h2>My Page</h2>
         </div>
     )
 }
