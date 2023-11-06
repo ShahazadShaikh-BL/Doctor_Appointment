75cd19bf2cf77d24feea9b67f39032b70e9c5bc1
shahazad08 <shaikh.shahazad@bridgelabz.com>
2023-11-06 17:05:50 +0530

    Added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Appbar from '../components/Appbar';
-import { Box } from "@mui/material";
+// import { Box } from "@mui/material";
 import Sidebar from '../components/Sidebar';
+import { Box, Grid, Card, CardContent, Typography, Button, CardActions } from "@mui/material";
+
 
 function Dashboard() {
     const [isLoading, setLoading] = useState(true);
@@ -9,8 +11,8 @@ function Dashboard() {
     const [error, setError] = useState(null);
     const [open, setOpen] = useState(false);
     const [search, setSearch] = useState("");
-    const [title, setTitle]=useState("Dr. Shaikh")
-   
+    const [title, setTitle] = useState("Dr. Shaikh")
+
 
     const getFetchUsers = () => {
         setLoading(true);
@@ -31,41 +33,63 @@ function Dashboard() {
         getFetchUsers();
     }, []); // Empty dependency array ensures this effect runs once on mount
 
+
+
     const handleSearch = (searchValue) => {
         console.log("Search Value", searchValue);
         setSearch(searchValue);
-      };
+    };
 
     const handleDrawerOpen = () => {
         console.log("App bar open", open);
-          setOpen((prevStates) => {
+        setOpen((prevStates) => {
             return !prevStates;
-          });
-        };
+        });
+    };
 
-        function handleTitle(title) {
-            console.log("Set Title", title);
-            setTitle(title);
-          }
-        
+    function handleTitle(title) {
+        console.log("Set Title", title);
+        setTitle(title);
+    }
+
     return (
         <Box >
-        <Appbar handleDrawerOpen={handleDrawerOpen} handleSearch={handleSearch} title={title} />
-        <Sidebar open={open} handleTitle={handleTitle}/>
-        <h1>All User</h1>
-             {error ? <p>{error.message}</p> : null}
-             {users.map(user => {
-                const { address, name, email } = user;
-                return (
-                    <div key={name}>
-                        <p>Name: {name}</p>
-                        <p>Email: {email}</p>
-                        <p>Address: {address}</p>
-                        <hr />
-                    </div>
-                );
-            })}
-      </Box>
+            <Appbar handleDrawerOpen={handleDrawerOpen} handleSearch={handleSearch} title={title} />
+            <Sidebar open={open} handleTitle={handleTitle} />
+
+            <h1>All User</h1>
+
+
+            <Box className="main-container">
+                <Grid container spacing={4}>
+                    {users.map(user => {
+                        const { id, address, name, specialist, portfolioImage } = user;
+                        return (
+                            <Grid item xs={12} sm={6} md={3} key={id}>
+                                <Card>
+                                    <CardContent>
+                                        <img src={`${portfolioImage}`} alt="" />
+                                        <Typography variant="h5">{name}</Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary" variant="body2">{specialist}</Typography>
+                                        <Typography sx={{ mb: 1.5 }} color="text.secondary" variant="body3">
+                                            {address}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button variant="contained" sx={{
+                                            display: "flex",
+                                            marginLeft: "91px",
+
+
+                                        }} size="small">View Profile</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </Box>
+        </Box>
         //
         //     
         // </React.Fragment>
